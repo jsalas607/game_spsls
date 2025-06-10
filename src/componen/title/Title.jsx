db4b9ddda9ac289299372cda7183ac10bcd149ad
julioJulioMarquezH <jmarquezh975@gmail.com>
2025-06-09 22:48:54 -0500

    fix

--- src/componen/title/Title.jsx
+++ src/componen/title/Title.jsx
@@ -1,16 +1,27 @@
 import styles from "@/src/componen/title/Title.module.css"
 
 const Title = () => {
+
+  const title = [
+    {
+      name: 'Piedra, Papel',
+    },
+    {
+      name: 'Tijera, Lagarto',
+    },
+    {
+      name: 'Spock',
+    },
+  ]
+
   return (
-<header className={styles.header}>
-  <h1 className="nes-text is-primary">
-    Piedra Papel
-  </h1>  <h1 className="nes-text is-primary">
-    Tijera Lagarto
-  </h1>  <h1 className="nes-text is-primary">
-    Spock
-  </h1>
-</header>
+    <header className={styles.header}>
+      {title.map((item) => (
+        <h1 key={item.name} className="nes-text is-primary">
+          {item.name}
+        </h1>
+      ))}
+    </header>
   )
 }
 
